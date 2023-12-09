4f5e887572952666f00a1ffdebb2b4acb686b3fc
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-12-09 22:47:27 +1100

    Cleaned up MotorOpMode.kt

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/MotorOpMode.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/MotorOpMode.kt
@@ -4,47 +4,29 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import dev.frozenmilk.dairy.calcified.Calcified
 import dev.frozenmilk.dairy.calcified.hardware.CalcifiedMotor
-import dev.frozenmilk.dairy.calcified.hardware.ZeroPowerBehaviour
 import dev.frozenmilk.dairy.core.DairyCore
 import dev.frozenmilk.dairy.core.FeatureRegistrar
 import dev.frozenmilk.dairy.core.OpModeLazyCell
+import kotlin.math.sin
 
 // enables all dairy features
 @DairyCore
 @TeleOp
 class MotorOpMode : OpMode() {
 
-	val motor: CalcifiedMotor by OpModeLazyCell {
+	private val motor: CalcifiedMotor by OpModeLazyCell {
 		Calcified.controlHub.motors.getMotor(0)
 	}
-//
-//	val motor1: CalcifiedMotor by OpModeLazyCell {
-//		Calcified.controlHub.motors.getMotor(1)
-//	}
-//
-//	val motorController: Controller<AngleRadians> by OpModeLazyCell{
-//		PController(
-//				MotorControllerGroup(listOf(motor)),
-//				Calcified.controlHub.encoders.getRadiansEncoder(0, 8192.0).positionSupplier,
-//				0.1
-//		)
-//	}
 
 	init {
-		// ensures that the feature flags to enable the MarrowMap are present, otherwise throws a helpful error
+		// ensures that the feature flags to enable Calcified are present, otherwise throws a helpful error
 		FeatureRegistrar.checkFeatures(this, Calcified)
 	}
 
 	override fun init() {
-//		OpModeLazyCell {}
-		Calcified.controlHub.motors.getMotor(0).zeroPowerBehavior = ZeroPowerBehaviour.BRAKE
-
-//		motorController.update(AngleRadians())
 	}
 
 	override fun loop() {
-//		if (gamepad1.a) motor.power = 1.0
-//		else if (gamepad1.b) motor.power = -1.0
-//		else motor.power = 0.0
+		motor.power = sin(runtime)
 	}
 }
