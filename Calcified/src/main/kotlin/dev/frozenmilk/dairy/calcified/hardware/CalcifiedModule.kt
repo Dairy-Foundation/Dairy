e8250e494b1e42d109288f17aad6da78f3aa94be
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-12-11 17:55:05 +1100

    Added imus to the calcified module hardware map

--- Calcified/src/main/kotlin/dev/frozenmilk/dairy/calcified/hardware/CalcifiedModule.kt
+++ Calcified/src/main/kotlin/dev/frozenmilk/dairy/calcified/hardware/CalcifiedModule.kt
@@ -4,6 +4,7 @@ import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.hardware.lynx.commands.core.LynxGetBulkInputDataCommand
 import com.qualcomm.hardware.lynx.commands.core.LynxGetBulkInputDataResponse
 import dev.frozenmilk.dairy.calcified.collections.Encoders
+import dev.frozenmilk.dairy.calcified.collections.IMUs
 import dev.frozenmilk.dairy.calcified.collections.Motors
 import dev.frozenmilk.dairy.calcified.collections.Servos
 
@@ -11,10 +12,12 @@ class CalcifiedModule(val lynxModule: LynxModule) {
 	val motors = Motors(this)
 	val encoders = Encoders(this)
 	val servos = Servos(this)
+	val imus = IMUs(this)
 	private var deviceMap: MutableMap<Class<*>, MutableMap<Byte, out Any>> = mapOf(
 			CalcifiedMotor::class.java to motors,
 			CalcifiedEncoder::class.java to encoders,
-			CalcifiedServo::class.java to servos
+			CalcifiedServo::class.java to servos,
+			CalcifiedIMU::class.java to imus,
 	).toMutableMap()
 
 	fun <T> unsafeGet(type: Class<out T>, port: Byte): T {
