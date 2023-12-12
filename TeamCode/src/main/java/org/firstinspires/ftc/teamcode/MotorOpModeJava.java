13b1b569b461a64d99d12237eef2ab1335a3f45b
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-12-12 21:01:38 +1100

    updated `MotorOpModeJava.java`

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOpModeJava.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorOpModeJava.java
@@ -1,31 +1,29 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//
-//import dev.frozenmilk.dairy.calcified.Calcified;
-//import dev.frozenmilk.dairy.calcified.hardware.CalcifiedMotor;
-//import dev.frozenmilk.dairy.calcified.hardware.ZeroPowerBehaviour;
-//import dev.frozenmilk.dairy.core.DairyCore;
-//import dev.frozenmilk.dairy.core.OpModeLazyCell;
-//
-//@DairyCore
-//@TeleOp
-//public class MotorOpModeJava extends OpMode {
-//	private final OpModeLazyCell<CalcifiedMotor> motor = new OpModeLazyCell<>(() -> {
-//		CalcifiedMotor result = Calcified.INSTANCE.getControlHub().getMotors().getMotor((byte) 1);
-//		result.setCachingTolerance(0.02);
-//		result.setZeroPowerBehavior(ZeroPowerBehaviour.FLOAT);
-//		return result;
-//	});
-//
-//	@Override
-//	public void init() {
-//		motor.get().setPower(1.0);
-//	}
-//
-//	@Override
-//	public void loop() {
-//
-//	}
-//}
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import dev.frozenmilk.dairy.calcified.Calcified;
+import dev.frozenmilk.dairy.calcified.hardware.CalcifiedMotor;
+import dev.frozenmilk.dairy.core.DairyCore;
+import dev.frozenmilk.dairy.core.FeatureRegistrar;
+import dev.frozenmilk.dairy.core.OpModeLazyCell;
+
+@DairyCore
+@TeleOp
+public class MotorOpModeJava extends OpMode {
+	public MotorOpModeJava() {
+		FeatureRegistrar.INSTANCE.checkFeatures(this, Calcified.INSTANCE);
+	}
+	
+	private final OpModeLazyCell<CalcifiedMotor> motor = new OpModeLazyCell<>(() -> Calcified.INSTANCE.getControlHub().getMotors().getMotor((byte) 1));
+
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public void loop() {
+		motor.get().setPower(Math.sin(getRuntime()));
+	}
+}
