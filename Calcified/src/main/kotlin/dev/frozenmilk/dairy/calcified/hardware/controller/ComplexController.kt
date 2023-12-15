bb87c8d33ca43832bd33126a315f83c829f08828
TechnoDrive <hextanium.on.discord@gmail.com>
2023-12-14 23:19:37 -0500

    refactor: move `LambdaController` into its own file from `ComplexController`

--- Calcified/src/main/kotlin/dev/frozenmilk/dairy/calcified/hardware/controller/ComplexController.kt
+++ Calcified/src/main/kotlin/dev/frozenmilk/dairy/calcified/hardware/controller/ComplexController.kt
@@ -51,27 +51,3 @@ interface ComplexController<IN> : Feature {
 		motors.power = 0.0
 	}
 }
-
-class LambdaController<IN> private constructor(override var target: IN, override val motors: SimpleMotor, override val calculate: Supplier<Double>) : ComplexController<IN> {
-	override val dependencies: Set<Dependency<*, *>> = DependencySet(this).yieldsTo(Calcified::class.java)
-
-	init {
-		FeatureRegistrar.registerFeature(this)
-	}
-
-	/**
-	 * constructs a new Controller that always outputs 0
-	 */
-	constructor(target: IN) : this(target, MotorControllerGroup(), { 0.0 })
-
-	fun addMotors(vararg motors: SimpleMotor): LambdaController<IN> = LambdaController(this.target, MotorControllerGroup(*motors), this.calculate)
-
-	fun <OUT> appendErrorBasedController(errorSupplier: ErrorSupplier<IN, OUT>, calculationComponent: (OUT) -> Double) = LambdaController(this.target, this.motors) { calculate.get() + calculationComponent(errorSupplier.getError(target)) }
-	fun <OUT> appendPositionBasedController(positionSupplier: Supplier<OUT>, calculationComponent: (OUT) -> Double) = LambdaController(this.target, this.motors) { calculate.get() + calculationComponent(positionSupplier.get()) }
-
-	fun appendPController(errorSupplier: ErrorSupplier<IN, Double>, kP: Double) = appendErrorBasedController(errorSupplier) { input: Double -> input * kP }
-
-	fun appendFFController(positionSupplier: Supplier<Double>, kFF: Double) = appendPositionBasedController(positionSupplier) { input: Double -> input * kFF }
-
-	fun appendAngleFFController(positionSupplier: Supplier<Angle>, kFF: Double) = appendPositionBasedController(positionSupplier) { input: Angle -> kFF * cos(input.intoRadians().theta) }
-}
