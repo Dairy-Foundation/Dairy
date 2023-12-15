30b732387255322c89959f7e6bc4a1946ead07ee
TechnoDrive <hextanium.on.discord@gmail.com>
2023-12-14 23:33:42 -0500

    add: `ProfiledController`, taking in a `Suppler<Double>` for `time`, based off of a `ProfileStateComponent`, returning a `LambdaController`

---
+++ Calcified/src/main/kotlin/dev/frozenmilk/dairy/calcified/hardware/controller/ProfiledController.kt
@@ -0,0 +1,9 @@
+package dev.frozenmilk.dairy.calcified.hardware.controller
+
+import dev.frozenmilk.dairy.calcified.hardware.SimpleMotor
+import dev.frozenmilk.util.profile.AsymmetricMotionProfile
+import dev.frozenmilk.util.profile.ProfileStateComponent
+import java.util.function.Supplier
+
+fun <IN> ProfiledController(target: IN, motors: SimpleMotor, profile: AsymmetricMotionProfile, component: ProfileStateComponent, time: Supplier<Double>)
+    = LambdaController(target, motors) { profile.calculate(time.get(), component) }
