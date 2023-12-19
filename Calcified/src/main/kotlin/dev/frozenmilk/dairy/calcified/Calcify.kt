94b8075a150a451c0a3f229ef07a432fda49e0d0
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-12-20 06:42:30 +1100

    fixed kdocs from true to false

--- Calcified/src/main/kotlin/dev/frozenmilk/dairy/calcified/Calcify.kt
+++ Calcified/src/main/kotlin/dev/frozenmilk/dairy/calcified/Calcify.kt
@@ -17,7 +17,7 @@ annotation class Calcify(
 		/**
 		 * Controls when [Calcified] drops its modules and this hardware objects
 		 *
-		 * Set to true if you want to prompt [Calcified] to drop its hardware objects by hand.
+		 * Set to false if you want to prompt [Calcified] to drop its hardware objects by hand.
 		 * This should be done using [Calcified.clearModules]
 		 *
 		 * By default, drops hardware objects at the start of an auto, and at the end of a teleop, allowing values to be carried over from an auto to a teleop
