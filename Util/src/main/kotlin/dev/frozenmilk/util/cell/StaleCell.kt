f54251d7fa662b47b6bcaa4891833d8280e12223
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-12-26 04:02:02 +1100

    improved invalidating cell, added stale cell

---
+++ Util/src/main/kotlin/dev/frozenmilk/util/cell/StaleCell.kt
@@ -0,0 +1,14 @@
+package dev.frozenmilk.util.cell
+
+import java.util.function.Supplier
+
+/**
+ * @param timeOut time in seconds after which the contents of the cell are no-longer valid, and so become stale and invalid, this timer gets reset after the contents get evaluated
+ */
+class StaleCell<T>(supplier: Supplier<T>, var timeOut: Double) : InvalidatingCell<T>(supplier) {
+	init {
+		invalidator = Supplier {
+			this.timeSinceAfterLastEval > timeOut
+		}
+	}
+}
