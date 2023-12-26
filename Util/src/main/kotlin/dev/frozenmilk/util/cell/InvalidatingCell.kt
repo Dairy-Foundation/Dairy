15055f07e85b3ee57dd41c956a191d1fa132ccd7
Froze-N-Milk <oscar.chevalier@gmail.com>
2023-12-26 21:09:14 +1100

    made invalidating cell safer to use

--- Util/src/main/kotlin/dev/frozenmilk/util/cell/InvalidatingCell.kt
+++ Util/src/main/kotlin/dev/frozenmilk/util/cell/InvalidatingCell.kt
@@ -2,7 +2,7 @@ package dev.frozenmilk.util.cell
 
 import java.util.function.Supplier
 
-open class InvalidatingCell<T>(supplier: Supplier<T>, var invalidator: Supplier<Boolean> = Supplier { false }) : LazyCell<T>(supplier) {
+open class InvalidatingCell<T>(supplier: Supplier<T>, var invalidator: Supplier<Boolean>) : LazyCell<T>(supplier) {
 	override fun get(): T {
 		if (invalidator.get()) invalidate()
 		return super.get()
--- Util/src/main/kotlin/dev/frozenmilk/util/cell/StaleCell.kt
+++ Util/src/main/kotlin/dev/frozenmilk/util/cell/StaleCell.kt
@@ -5,7 +5,7 @@ import java.util.function.Supplier
 /**
  * @param timeOut time in seconds after which the contents of the cell are no-longer valid, and so become stale and invalid, this timer gets reset after the contents get evaluated
  */
-class StaleCell<T>(supplier: Supplier<T>, var timeOut: Double) : InvalidatingCell<T>(supplier) {
+class StaleCell<T>(supplier: Supplier<T>, var timeOut: Double) : InvalidatingCell<T>(supplier, Supplier { false }) {
 	init {
 		invalidator = Supplier {
 			this.timeSinceAfterLastEval > timeOut
