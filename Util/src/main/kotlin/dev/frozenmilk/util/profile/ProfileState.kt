53784c8ce5beffaf54d6901a2a2574587435b9f0
Froze-N-Milk <oscar.chevalier@gmail.com>
2024-01-30 12:36:15 +1100

    fix: updates across the system, submodularisation

--- Util/src/main/kotlin/dev/frozenmilk/util/profile/ProfileState.kt
+++
@@ -1,9 +0,0 @@
-package dev.frozenmilk.util.profile
-
-data class ProfileState(val x: Double, val v: Double, val a: Double)
-
-enum class ProfileStateComponent {
-    Position,
-    Velocity,
-    Acceleration
-}
--- Util/src/main/kotlin/dev/frozenmilk/util/timeblock/TimedBlocking.kt
+++
@@ -1,9 +0,0 @@
-package dev.frozenmilk.util.timeblock
-
-class TimedBlocking(duration: Double, toRun: Runnable = Runnable {} ) {
-	init {
-		val durationNano = duration * 1E9
-		val startTime = System.nanoTime()
-		while (System.nanoTime() - startTime < durationNano) { toRun.run() }
-	}
-}
